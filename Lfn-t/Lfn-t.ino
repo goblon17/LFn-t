ffc5d761f32e0cff180741b269d0ac788e59d814
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2023-01-01 19:39:52 +0100

    Create logic for main menu and game over

--- Lfn-t/Lfn-t.ino
+++ Lfn-t/Lfn-t.ino
@@ -17,6 +17,29 @@
 
 #define isPressed(x) (digitalRead(x)==LOW)
 
+#define MAX_ROUNDS_NUMBER 999
+
+void menuLoop();
+void preLoop();
+void gameLoop();
+void overLoop();
+
+void resetGame();
+
+enum scene_e {
+  MAIN_MENU,
+  PRE_GAME,
+  GAME,
+  GAME_OVER
+};
+
+scene_e scene;
+
+int roundsNumber = 3;
+int roundsPlayed = 0;
+int player1Score = 0;
+int player2Score = 0;
+
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 
 void setup() {
@@ -41,9 +64,92 @@ void setup() {
   digitalWrite(LED_GREEN, LOW);
   digitalWrite(LED_BLUE, LOW);
   digitalWrite(LED_YELLOW, LOW);
+
+  // Setup LCD
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.clear();
+  
+  scene = MAIN_MENU;
 }
 
 void loop() {
+  switch (scene) {
+    case MAIN_MENU:
+      menuLoop();
+      break;
+    case PRE_GAME:
+      preLoop();
+      break;
+    case GAME:
+      gameLoop();
+      break;
+    case GAME_OVER:
+      overLoop();
+      break;
+  }
+}
 
+void menuLoop() {
+  lcd.clear();
+  lcd.print("      LFn't     ");
+  lcd.print("    Start (G)   ");
+  if (isPressed(P1_GREEN) || isPressed(P2_GREEN)) {
+    scene = PRE_GAME;
+  }
+}
 
+void preLoop() {
+  lcd.clear();
+  lcd.print("   Rounds (G)   ");
+  lcd.print("<R            B>");
+  char buf[4];
+  itoa(roundsNumber, buf, 10);
+  int pos = 8 - (strlen(buf) / 2);
+  lcd.setCursor(pos, 1);
+  lcd.printstr(buf);
+
+  if (isPressed(P1_BLUE) || isPressed(P2_BLUE)) {
+    if (roundsNumber < MAX_ROUNDS_NUMBER) {
+      roundsNumber ++;
+    }
+  }
+
+  if (isPressed(P1_RED) || isPressed(P2_RED)) {
+    if (roundsNumber > 1) {
+      roundsNumber --;
+    }
+  }
+
+  if (isPressed(P1_GREEN) || isPressed(P2_GREEN)) {
+    scene = GAME;
+  }
+}
+
+void gameLoop() {
+
+}
+
+void overLoop() {
+  lcd.clear();
+  if (player1Score > player2Score) {
+    lcd.print("   Winner: P1   ");
+  }
+  else if (player1Score < player2Score) {
+    lcd.print("   Winner: P2   ");
+  }
+  else if (player1Score == player2Score) {
+    lcd.print("       Tie      ");
+  }
+  lcd.print("  Continue (G)  ");
+  if (isPressed(P1_GREEN) || isPressed(P2_GREEN)) {
+    resetGame();
+    scene = MAIN_MENU;
+  }
+}
+
+void resetGame() {
+  roundsPlayed = 0;
+  player1Score = 0;
+  player2Score = 0;  
 }
